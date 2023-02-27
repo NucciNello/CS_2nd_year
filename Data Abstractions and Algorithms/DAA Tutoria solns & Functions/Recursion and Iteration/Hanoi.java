776245e98992f8708dc00ddb0c848f35190d51ae
nuhu <caljr9301@gmail.com>
2023-02-27 16:33:21 +0300

    ...

--- Data Abstractions and Algorithms/DAA Tutoria solns & Functions/Recursion and Iteration/Hanoi.java
+++ Data Abstractions and Algorithms/DAA Tutoria solns & Functions/Recursion and Iteration/Hanoi.java
@@ -5,10 +5,11 @@ public class Hanoi {
     static void hanoi(int n, char source, char destination, char temp_rod){
         if (n==1){
           System.out.println("Move disk 1 from rod "+source+ " to rod "+destination); 
+        }else{
+            hanoi(n-1, source, temp_rod, destination);
+            System.out.println("Move disk "+n+" from rod "+source+" to rod "+destination);
+            hanoi(n-1, temp_rod, destination, source);
         }
-        hanoi(n-1, source, temp_rod, destination);
-        System.out.println("Move disk "+n+" from rod "+source+" to rod "+destination);
-        hanoi(n-1, temp_rod, destination, source);
     }
 
     public static void main(String[] args) {
