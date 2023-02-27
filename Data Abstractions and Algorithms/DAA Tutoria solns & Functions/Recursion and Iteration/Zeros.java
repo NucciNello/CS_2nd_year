a5efaf3a296bbb2a1e0a35e84eadc901458dc795
nuhu <caljr9301@gmail.com>
2023-02-27 15:24:15 +0300

    Number of Zeros Recursion and Iteration

--- Data Abstractions and Algorithms/DAA Tutoria solns & Functions/Recursion and Iteration/Zeros.java
+++ Data Abstractions and Algorithms/DAA Tutoria solns & Functions/Recursion and Iteration/Zeros.java
@@ -1,11 +1,24 @@
 import java.util.Scanner;
 public class Zeros {
-    static int zeros(int n){
+
+    //Recursio function
+    static int recu_zeros(int n){
         if (n == 0)
             return 1;
         if ((-10<n) && (10>n))
             return 0;
-        return zeros((n)/10) + zeros(((n)%10)); 
+        return recu_zeros((n)/10) + recu_zeros(((n)%10)); 
+    }
+    //Iteration function
+    static int itera_zeros(int n){
+        int count = 0;
+        while(n>0||n<0){
+            if(n%10==0){
+                count++;
+            }
+            n /= 10;
+        }
+        return count;
     }
 
     public static void main(String[] args){
@@ -14,7 +27,8 @@ public class Zeros {
         System.out.println("Enter the Intger:");
         int number = sc.nextInt();
 
-        System.out.println(zeros(number));
+        System.out.println(recu_zeros(number));
+        System.out.println(itera_zeros(number));
 
 
     }
