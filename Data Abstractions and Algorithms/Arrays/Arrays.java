7e59fc8b4490f5087193414b55e8ea8deae0cb0d
nuhu <caljr9301@gmail.com>
2023-02-27 23:00:32 +0300

    Directory changes

---
+++ Data Abstractions and Algorithms/Arrays/Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays{
+  public static void main(String[] args) {
+
+    int[][] matrix = {
+      {1, 2, 3, 4, 5},
+      {2, 3, 4, 5},
+      {3, 4, 5},
+      {4, 5},
+      {5}
+    }; 
+  
+        System.out.println(matrix.length);
+        System.out.println(matrix[3].length);
+    }
+}
