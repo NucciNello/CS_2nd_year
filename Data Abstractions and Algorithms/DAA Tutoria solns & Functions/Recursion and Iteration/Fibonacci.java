e073af767bc6ad429bb6bbb2505a3e710e4c4ac5
nuhu <caljr9301@gmail.com>
2023-02-27 12:56:22 +0300

    explanation on Arrya-Iteration

--- Data Abstractions and Algorithms/DAA Tutoria solns & Functions/Recursion and Iteration/Fibonacci.java
+++ Data Abstractions and Algorithms/DAA Tutoria solns & Functions/Recursion and Iteration/Fibonacci.java
@@ -28,8 +28,10 @@ public class Fibonacci {
         }
     }
 
-    //Array-Iterative function
-    static int fibonacci(int n){
+    /***Array-Iterative function
+    the problem with this it only put values to index 3, leaving
+        other indeces with a 0 as their values***/
+    static int array_fibonacci(int n){
         int[] f = new int[100];
         f[0] = 0;
         f[1] = 1;
@@ -47,6 +49,8 @@ public static void main(String[] args) {
 
     System.out.println(recu_fibonacci(n));
     System.out.println(itera_fibonacci(n));
+    System.out.println(array_fibonacci(n));
+
     
 }
 }
