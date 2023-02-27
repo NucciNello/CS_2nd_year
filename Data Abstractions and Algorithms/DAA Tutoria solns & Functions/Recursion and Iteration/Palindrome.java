d41af8efa8f394d6ab54ac6feeb3248884622a98
nuhu <caljr9301@gmail.com>
2023-02-27 16:23:53 +0300

    Palindrome Recursion and Iteration

--- Data Abstractions and Algorithms/DAA Tutoria solns & Functions/Recursion and Iteration/Palindrome.java
+++ Data Abstractions and Algorithms/DAA Tutoria solns & Functions/Recursion and Iteration/Palindrome.java
@@ -1,16 +1,27 @@
 import java.util.Scanner;
 public class Palindrome {
-    static boolean isPalindrome( String word, int first, int last){
-        boolean answer = true;
-        if(word.charAt(first)==word.charAt((last))){
+    static boolean recu_palindrome( String word, int first, int last){
+        if(word.charAt(first)==word.charAt(last)){
             if(first <= last){
-                isPalindrome(word, first + 1, last-1);
+                recu_palindrome(word, first + 1, last-1);
             }
         }
         else{
-            answer = false;
+            return false;
         }
-        return answer;
+        return true;
+    }
+
+    static boolean itera_palindrome(String word, int first, int last){
+        while(first<last){
+            if(word.charAt(first)==word.charAt(last)){
+                last--;
+                first++;
+                return true;
+            }
+            return false;
+        }
+        return false;
     }
 
     public static void main(String[] args){
@@ -19,13 +30,13 @@ public class Palindrome {
         // System.out.println("type the text here!");
         // String [] message = scan.nextLine();
 
-        String message = "madam";
+        String message = "emadame";
 
         // int first = 0;
         // int last = message.length();
 
-
-        System.out.println(isPalindrome(message, 0, message.length()-1));
+        System.out.println(recu_palindrome(message, 0, message.length()-1));
+        System.out.println(itera_palindrome(message, 0, message.length()-1));
 
     
 }
